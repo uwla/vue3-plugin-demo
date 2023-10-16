986d707e817b1752f5ed97f0511ee843b0cc73d2
uwla <uwlalinux@gmail.com>
2023-10-15 21:27:45 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Vue3 Plugin Demo App</h1>
+  <p>This is the demo app.</p>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
